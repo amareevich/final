4cd8ada84981de7a7cffb856c18d97cf92358750
Amareevich <amareevich@example.com>
2025-07-19 00:16:49 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,30 @@
+import './style/style.scss';
+import { modal_view } from './js/modal.js';
+
+modal_view();
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    let mode = null;
+    let swiperInst, desktopInst;
+
+    async function setListener() {
+
+        const newMode = window.innerWidth < 767 ? 'modile' : 'desktop';
+
+        if (newMode === mode) return;
+        mode = newMode;
+
+        if (newMode === 'modile') {
+            desktopInst?.forEach(el => el.destroy());
+            swiperInst = (await import('./js/swiper.js')).default();
+        } else if (newMode === 'desktop') {
+            swiperInst?.forEach(el => el.destroy());
+            desktopInst = (await import('./js/view_element.js')).default();
+        }
+    }
+
+    setListener();
+    window.addEventListener('resize', setListener);
+
+})
